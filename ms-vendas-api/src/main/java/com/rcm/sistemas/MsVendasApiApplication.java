5457f2c256fe1e596a8dd67467d9c74e511cdae2
= <=>
2022-11-15 19:41:25 -0300

    Commit Initial

---
+++ ms-vendas-api/src/main/java/com/rcm/sistemas/MsVendasApiApplication.java
@@ -0,0 +1,13 @@
+package com.rcm.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVendasApiApplication.class, args);
+	}
+
+}
